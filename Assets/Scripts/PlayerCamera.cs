176c29a4ee5882233d213f76e02ee2b7b63492b7
Jakov <jakov.mucnjak@gmail.hr>
2019-11-25 22:32:04 +0100

    Camera smooth follow Player

---
+++ Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public float smoothSpeed;
+
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 playerPosition = new Vector3(transform.position.x, transform.position.y + 3, mainCamera.transform.position.z);
+        mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, playerPosition, Time.deltaTime * smoothSpeed);
+    }
+}
