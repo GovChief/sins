bf4c3c4c5d31dfcefed0e6f621c223da0a84784c
Jure <>
2020-01-29 23:52:26 +0100

    BulletSpawner i Trap skripte

---
+++ Assets/Scripts/BulletSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpawner : MonoBehaviour
+{
+    public BulletController bulletPrefab;
+    public float bulletSpeed;
+    private float time = 0;
+    public float bulletDelay;
+
+    public bool shoot;
+
+    private Vector2 dir2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dir2 = new Vector2(bulletSpeed, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(shoot == true)
+        {
+            if(time< Time.time)
+            {
+                Vector2 position = new Vector2(transform.position.x, transform.position.y+1);
+                BulletController bullet = Instantiate(bulletPrefab, position, transform.rotation) as BulletController;
+                bullet.dir = dir2;
+                time = Time.time + bulletDelay;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerDamageTriggerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDamageTriggerController : MonoBehaviour
+{
+    public int damage = 10;
+    public int health = 100;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        if (other.gameObject.name == "Trap")
+        {
+            health -= 10;
+        } else if (other.tag == "Bullet")
+        {
+            health -= 15;
+        }
+    }
+}
