e94ae854f055195eaf1586838c2e1aadf4f6c475
Matija Hanzekovic <hanzyy.kc@gmail.com>
2020-01-29 09:32:08 +0100

    Implemented Node for pathfinding

---
+++ Assets/Scripts/AI/Node.cs.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : IHeapItem<Node>
+{
+
+    public bool walkable;
+    public int gridX;
+    public int gridY;
+    public Vector2 worldPosition;
+
+    public int gCost;
+    public int hCost;
+    int heapIndex;
+    public Node parent;
+
+    public Node(bool walkable, Vector2 worldPos, int gridX, int gridY)
+    {
+        this.walkable = walkable;
+        this.worldPosition = worldPos;
+        this.gridX = gridX;
+        this.gridY = gridY;
+    }
+
+    public int fCost
+    {
+        get
+        {
+            return gCost + hCost;
+        }
+    }
+
+    public int HeapIndex
+    {
+        get
+        {
+            return heapIndex;
+        }
+        set
+        {
+            heapIndex = value;
+        }
+    }
+
+    public int CompareTo(Node nodeToCompare)
+    {
+        int compare = fCost.CompareTo(nodeToCompare.fCost);
+
+        if (compare == 0)
+        {
+            compare = hCost.CompareTo(nodeToCompare.hCost);
+        }
+
+        return -compare;
+    }
+}
